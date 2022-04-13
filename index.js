1ee878236b894b72681a142759940635f6ee98ca
RobiA17 <ambarusrobert@gmial.com>
2022-04-13 23:05:50 +0300

    main page design updated

--- index.js
+++ index.js
@@ -1,7 +1,53 @@
 $(document).ready(function() {
 
-    $("heder-title-span").animate({
-        fontSize: '3.5em'
+    let descriptionAnimation = 0;
+    let buttonAnimation = 0;
+
+    $(window).scroll(function() {
+        var description = $(".description-container-hidden");
+        var buttonLeft = $(".left-button");
+        var buttonRight = $(".right-button");
+        var title = $(".title-container");
+        var mouse = $(".mouse-scroll");
+        var windowpos = $(window).scrollTop();
+
+        if (windowpos >= 100) {
+            descriptionAnimation = 1;
+            description.addClass("description-container-revealed");
+
+            title.addClass("title-container-hidden");
+
+        } else {
+            if (descriptionAnimation == 1) {
+                description.removeClass("description-container-revealed");
+                description.addClass("off");
+                descriptionAnimation = 0;
+
+                title.removeClass("title-container-hidden");
+            }
+        }
+
+        if (windowpos >= 170) {
+            buttonAnimation = 1;
+            buttonLeft.addClass("visible-left");
+            buttonRight.addClass("visible-right");
+
+            buttonLeft.removeClass("dissapear-left");
+            buttonRight.removeClass("dissapear-right");
+
+            mouse.addClass("mouse-scroll-hidden");
+        } else {
+            if (buttonAnimation == 1) {
+                buttonLeft.removeClass("visible-left");
+                buttonRight.removeClass("visible-right");
+
+                buttonLeft.addClass("dissapear-left");
+                buttonRight.addClass("dissapear-right");
+
+                mouse.removeClass("mouse-scroll-hidden");
+                buttonAnimation = 0;
+            }
+        }
+
     });
-    console.log('asd');
 });
