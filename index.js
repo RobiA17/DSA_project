c972bf734c665966a49662e5d271689e7aef21f6
RobiA17 <ambarusrobert@gmial.com>
2022-05-04 08:06:16 +0300

    animation page bases and

--- index.js
+++ index.js
@@ -1,6 +1,47 @@
-$(document).ready(function() {
+$(document).ready(async() => {
 
-    loadData("main-page-content.html");
+    class ListNode {
+        constructor(data) {
+            this.data = data
+            this.next = null
+        }
+    }
+
+    class LinkedList {
+        constructor(head = null) {
+            this.head = head
+        }
+
+        size() {
+            let count = 0;
+            let node = this.head;
+            while (node) {
+                count++;
+                node = node.next
+            }
+            return count;
+        }
+
+        clear() {
+            this.head = null;
+        }
+
+        getLast() {
+            let lastNode = this.head;
+            if (lastNode) {
+                while (lastNode.next) {
+                    lastNode = lastNode.next
+                }
+            }
+            return lastNode
+        }
+
+        getFirst() {
+            return this.head;
+        }
+    }
+
+    await loadData("pages/main_page.html");
     loadAllData();
 
     let descriptionAnimation = 0;
@@ -8,7 +49,7 @@ $(document).ready(function() {
     var index = 0;
     var title = $(".title-container");
     var body = $("#main-body");
-    var learnPage = $("#learn-page");
+    var learnPage = $("#learn_page");
     var mainContainer = $("#main-container");
     var menu = $(".menu");
     var menuButton = $(".menu-icon");
@@ -20,12 +61,88 @@ $(document).ready(function() {
     var description;
     var buttonLeft;
     var buttonRight;
+    var learnContainer;
+    $(document).click(function(event) {
+        var $target = $(event.target);
+        if ((!$target.closest('.menu').length && !$target.closest('.menu-icon').length) || ($target.closest('.menu-button').length)) {
+            closeMenu();
+        }
+    });
+
+    function testLinkedList() {
+        let node1 = new ListNode(2)
+        let node2 = new ListNode(5)
+        node1.next = node2
+
+        let list = new LinkedList(node1)
+
+        console.log(list.head.next.data)
+
+        console.log(list.getFirst())
+        console.log(list.getLast())
+        console.log(list.size())
+        list.clear()
+        console.log(list.size())
+    }
+
+    function initLearnPage() {
+        learnContainer = $(".learn-container");
+        var clicked = false;
+        var linkedListInfoButton = $("#linked-list-info-button");
+        var linkedListAnimatiobButton = $("#linked-list-animation-button");
+
+        linkedListInfoButton.click(() => {
+
+            if (!clicked) {
+                $.get("lists/linked-list/linked_list.html", function(data) {
+                    $("#linked-desc").append(data);
+                });
+                downButtonToUp('#linked-list-info-button');
+                clicked = !clicked;
+            } else {
+                $("#linked-desc").empty();
+                upButtonToDown('#linked-list-info-button');
+                clicked = !clicked;
+            }
+        });
+
+        linkedListAnimatiobButton.click(() => {
+            $('.pop-up').addClass('display-block');
+            $('body').addClass('no-scroll');
+            $('html').addClass('no-scroll');
+            $.get("lists/linked-list/linked_list_basic_animation.html", function(data) {
+                $(".pop-up-content").append(data);
+            });
+        });
+
+        $('.fa-xmark').click(() => {
+            $('.pop-up-content').empty();
+            $('.pop-up').removeClass('display-block');
+            $('body').removeClass('no-scroll');
+            $('html').removeClass('no-scroll');
+        });
+
+    }
+
+
+
+    function downButtonToUp(id) {
+        $(id + " i").removeClass("fa-chevron-down");
+        $(id + " i").addClass("fa-chevron-up");
+        $(id + ' span').text('Less info');
+    }
+
+    function upButtonToDown(id) {
+        $(id + " i").removeClass("fa-chevron-up");
+        $(id + " i").addClass("fa-chevron-down");
+        $(id + ' span').text('More info');
+    }
 
     function loadAllData() {
         setTimeout(function() {
             title = $(".title-container");
             body = $("#main-body");
-            learnPage = $("#learn-page");
+            learnPage = $("#learn_page");
             mainContainer = $("#main-container");
             menu = $(".menu");
             menuButton = $(".menu-icon");
@@ -43,26 +160,30 @@ $(document).ready(function() {
                 mainBodyToRightOut();
 
                 setTimeout(
-                    function() {
+                    async() => {
                         clearMainBody();
-                        loadData("learn-page.html");
+                        await loadData("pages/learn_page.html");
+                        initLearnPage();
                         mainBodyToLeftIn();
                     }, 200);
                 index = 1;
             });
 
             buttonLeft.click(() => {
+                index = 2;
                 mainBodyToLeftOut();
 
                 setTimeout(
-                    function() {
+                    async() => {
                         clearMainBody();
-                        loadData("animation-page.html");
+                        await loadData("pages/animation_page.html");
                         mainBodyToLeftIn();
                     }, 200);
-                index = 2;
             });
 
+            if (index == 1) {
+                console.log('asd');
+            }
 
         }, 100);
     }
@@ -115,16 +236,17 @@ $(document).ready(function() {
     }
 
     function loadData(from) {
-
-        $.get(from, function(data) {
-            body.append(data);
+        return new Promise(resolve => {
+            $.get(from, function(data) {
+                $("#main-body").append(data);
+                resolve('resolved');
+            });
         });
     }
 
     function clearMainBody() {
         body.empty();
     }
-
 
     $(window).scroll(function() {
         var windowpos = $(window).scrollTop();
@@ -154,7 +276,15 @@ $(document).ready(function() {
             buttonRight.removeClass("dissapear-right");
 
             mouse.addClass("mouse-scroll-hidden");
+
+            if (index == 1) {
+                learnContainer.addClass("visible-left");
+            }
+
         } else {
+            if (index == 1) {
+                learnContainer.removeClass("visible-left");
+            }
             if (buttonAnimation == 1) {
                 buttonLeft.removeClass("visible-left");
                 buttonRight.removeClass("visible-right");
@@ -170,17 +300,12 @@ $(document).ready(function() {
     });
 
 
-
-
     function closeMenu() {
         menu.removeClass("menu-appear");
         menu.addClass("menu-hide");
         menuButton.removeClass("menu-icon-hide");
         menuButton.addClass("menu-icon-appear");
     }
-
-
-    exitButton.click(() => closeMenu());
 
     menuButton.click(function openMenu() {
         menu.addClass("menu-appear");
@@ -189,39 +314,17 @@ $(document).ready(function() {
     });
 
     homeButton.click(() => {
-        closeMenu();
         window.scrollTo({
             top: 0,
             behavior: 'smooth'
         });
         if (index != 0) {
             setTimeout(
-                function() {
-                    setTimeout(() => {
+                () => {
+                    setTimeout(async() => {
                         mainBodyDownIn();
                         clearMainBody();
-                        loadData("main-page-content.html");
-                        loadAllData();
-                    }, 500);
-                }, 500);
-            mainBodyDownOut();
-            index = 0;
-        }
-    });
-
-    homeButton.click(() => {
-        closeMenu();
-        window.scrollTo({
-            top: 0,
-            behavior: 'smooth'
-        });
-        if (index != 0) {
-            setTimeout(
-                function() {
-                    setTimeout(() => {
-                        mainBodyDownIn();
-                        clearMainBody();
-                        loadData("main-page-content.html");
+                        await loadData("pages/main_page.html");
                         loadAllData();
                     }, 500);
                 }, 500);
@@ -231,44 +334,41 @@ $(document).ready(function() {
     });
 
     learnButton.click(() => {
-        closeMenu();
         window.scrollTo({
             top: 0,
             behavior: 'smooth'
         });
         if (index != 1) {
-            setTimeout(
-                function() {
-                    setTimeout(() => {
-                        mainBodyDownIn();
-                        clearMainBody();
-                        loadData("learn-page.html");
-                        loadAllData();
-                    }, 500);
-                }, 500);
+            setTimeout(async() => {
+                mainBodyDownIn();
+                clearMainBody();
+                await loadData("pages/learn_page.html");
+                // loadAllData();
+                initLearnPage();
+            }, 500);
             mainBodyDownOut();
             index = 1;
         }
     });
 
     animationButton.click(() => {
-        closeMenu();
+        testLinkedList();
         window.scrollTo({
             top: 0,
             behavior: 'smooth'
         });
         if (index != 2) {
+            index = 2;
             setTimeout(
-                function() {
-                    setTimeout(() => {
+                () => {
+                    setTimeout(async() => {
                         mainBodyDownIn();
                         clearMainBody();
-                        loadData("animation-page.html");
+                        await loadData("pages/animation_page.html");
                         loadAllData();
                     }, 500);
                 }, 500);
             mainBodyDownOut();
-            index = 2;
         }
     });
 
